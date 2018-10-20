2eb1b79c3d61f6970c2208fff3320590e604d218
Tanmay <tanmaymhatre40@gmail.com>
2018-10-20 18:19:55 +0530

    'led_control'

--- car_counter_US.js
+++ car_counter_US.js
@@ -1,30 +1,37 @@
 const five = require("johnny-five");
-const board = new five.Board();
+const board = new five.Board({
+      port: "COM19"
+});
 // const PubNub = require('pubnub');
 
 let frameDifference = 0;
 let lastFrame = 2000;
 let currentFrame = 0;
 let carsCount = 0;
+
 board.on("ready", function (){
     let ultrasonic = new five.Proximity({
         controller: "HCSR04",
         pin: 7
     });
 
+    var ledon = new five.Led(4);
+    var ledoff = new five.Led(5);
+    ledoff.on()
+function togggle(){
+ledon.toggle();ledoff.toggle();
+}
     ultrasonic.on("change", async function () {
         currentFrame = this.cm;
         if (this.cm < 50) {
           frameDifference = lastFrame - currentFrame;
           if (frameDifference > 100) {
             carsCount++;
-            // var publishConfig = {
-            //   channel: "",
-            //   message: {
-            //     carsCount:carsCount,
-            //   }
-            // }
-            // pubnub.publish(publishConfig)
+	if (carsCount>10){
+		togggle();
+		setTimeout(function(){togggle();},1000);
+		carsCount=0;
+	}
             console.log(carsCount);
           }
         }
